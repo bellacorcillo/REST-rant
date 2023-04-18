734109de37d493a01b2e64b7ab090a8586d1dd4a
bellacorcillo <isabellacorcillo@gmail.com>
2023-04-18 09:40:29 -0700

    adding pages

--- index.js
+++ index.js
@@ -17,7 +17,23 @@ app.get('*', (req, res) => {
 
 // GET /places
 app.get('/', (req, res) => {
-    res.render('places/index')
-  })  
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+      res.render('places/index', { places })
+
+  })
+  
 
 app.listen(process.env.PORT)
