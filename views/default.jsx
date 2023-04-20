8b67c4abba75d4bb3d0093fd855c133121d9364e
bellacorcillo <isabellacorcillo@gmail.com>
2023-04-19 21:25:31 -0700

    changes

--- views/default.jsx
+++ views/default.jsx
@@ -5,7 +5,7 @@ function Def(html) {
         <html>
             <head>
                 <title>Title</title>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossOrigin="anonymous"></link>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"></link>
                 <link rel="stylesheet" href="/css/style.css"></link>
             </head>
             <body>
@@ -16,9 +16,6 @@ function Def(html) {
                         </li>
                         <li>
                             <a href="/places">Places</a>
-                        </li>
-                        <li>
-                            <a href="/places/new">Add Place</a>
                         </li>
                     </ul>
                 </nav>
