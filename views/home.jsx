1e80ca12fc07db9a8109d5126b971562d52fc333
bellacorcillo <isabellacorcillo@gmail.com>
2023-04-19 21:13:47 -0700

    adding styles to rest-rant

--- views/home.jsx
+++ views/home.jsx
@@ -1,14 +1,23 @@
 const React = require('react')
 const Def = require('./default')
 
-function home () {
+function home() {
     return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Def>
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                    <img src="/images/homefood.jpg" alt="Outdoor Picnic" />
+                    <div>
+                        Photo by <a href="https://unsplash.com/@spencerdavis">Spencer Davis</a> on <a href="https://unsplash.com/photos/vJsj-hgOEG0">Unsplash</a>
+                    </div>
+                </div>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
     )
-  }
+}
 
 module.exports = home
