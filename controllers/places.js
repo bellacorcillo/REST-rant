f5326bcbc8d3b709c59971b5e0879b0bffb00dd0
bellacorcillo <isabellacorcillo@gmail.com>
2023-04-22 09:07:48 -0700

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,29 +1,21 @@
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-    db.Place.find()
-        .then((places) => {
-            res.render('places/index', { places })
-        })
-        .catch(err => {
-            console.log(err)
-            res.render('error404')
-        })
-})
-
-let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: 'http://placekitten.com/250/250'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: 'http://placekitten.com/250/250'
-  }]
+router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
+  
 
   router.get('/new', (req, res) => {
     res.render('places/new')
@@ -33,6 +25,8 @@ let places = [{
     console.log(req.body)
     res.send('POST /places')
   })
+
+  
   
   
 
