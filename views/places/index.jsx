39ab2ca50f80d84ade7dfcd2802ffb4b816fcf51
bellacorcillo <isabellacorcillo@gmail.com>
2023-04-29 11:17:06 -0700

    bonus changes

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -4,20 +4,12 @@ const Def = require('../default');
 function index(data) {
   let placesFormatted = data.places.map((place) => {
     return (
-      <div className="col-sm-6">
-        <h2>
-          <a href={`/places/${place.id}`}>{place.name}</a>
-        </h2>
-        <p className="text-center">{place.cuisines}</p>
-        <img src={place.pic} alt={place.name} />
-        <p className="text-center">
-          Located in {place.city}, {place.state}
-        </p>
-        <p className="text-center">
-          <a href={`/places/${place.id}`} className="btn btn-primary" title="View details">
-            View
-          </a>
-        </p>
+      <div className="col-sm-6 col-md-4 col-lg-3">
+        <a href={`/places/${place.id}`}>
+          <img src={place.pic} alt={place.name} />
+          <h2>{place.name}</h2>
+          <p>{place.description}</p>
+        </a>
       </div>
     );
   });
